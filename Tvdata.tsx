cd1d2ea0e7514bbe67820b1d9caf1bfa1d1d4cf4
1009557889@qq.com <1009557889@qq.com>
2019-03-23 16:20:56 +0800

    电视数据

---
+++ Tvdata.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+class Tvdata {
+
+    public sections =[
+        { key: "A", data: [{ title: "AOC", icon: "" },{title:'艾洛维',icon:''}] },
+        { key: "B", data: [{ title: "暴风TV", icon: "" }] },
+        { key: "C", data: [{ title: "创维", icon: "" },{ title: "创佳", icon: "" },{ title: "长虹", icon: "" }] },
+        { key: "D", data: [{ title: "东芝", icon: "" }] },
+        { key: "E", data: [{ title: "", icon: "" }] },
+        {
+            key: "F",
+            data: [{ title: "风行", icon: "" }, { title: "富可视", icon: "" }, { title: "飞利浦", icon: "" }]
+        },
+        { key: "G", data: [{ title: "", icon: "" }] },
+        {
+            key: "H",
+            data: [
+            { title: "HKC", icon: "" },
+            { title: "海信", icon: "" },
+            { title: "海尔", icon: "" }
+            ]
+        },
+        { key: "I", data: [{ title: "", icon: "" }] },
+        {
+            key: "J",
+            data: [{ title: "JVC", icon: "" }, { title: "聚力", icon: "" },{title:'京东方',icon:''}]
+        },
+        {
+            key: "K",
+            data: [{ title: "酷开", icon: "" }, { title: "康佳", icon: "" }]
+        },
+        {
+            key: "L",
+            data: [
+            { title: "LG", icon: "" },
+            { title: "乐视", icon: "" },
+            { title: "联想", icon: "" },
+            { title: "雷鸟", icon: "" }
+            ]
+        },
+        { key: "M", data: [{ title: "", icon: "" }] },
+        { key: "N", data: [{ title: "努比亚", icon: "" }] },
+        { key: "O", data: [{ title: "", icon: "" }] },
+        {
+            key: "P",
+            data: [{ title: "PPTV", icon: "" }, { title: "苹果", icon: "" }]
+        },
+        { key: "Q", data: [{ title: "清华同方", icon: "" }] },
+        { key: "R", data: [{ title: "", icon: "" }] },
+        {
+            key: "S",
+            data: [
+            { title: "三星", icon: "" },
+            { title: "索尼", icon: "" },
+            { title: "松下", icon: "" }
+            ]
+        },
+        { key: "T", data: [{ title: "TCL", icon: "" }] },
+        { key: "U", data: [{ title: "", icon: "" }] },
+        { key: "V", data: [{ title: "", icon: "" }] },
+        { key: "W", data: [{ title: "微鲸", icon: "" }] },
+        {
+            key: "X",
+            data: [
+            { title: "夏普", icon: "" },
+            { title: "夏新", icon: "" },
+            { title: "熊猫", icon: "" },
+            { title: "现代", icon: "" }
+            ]
+        },
+        { key: "Y", data: [{ title: "", icon: "" }] },
+        { key: "Z", data: [{ title: "", icon: "" }] }
+        ];
+}
+
+export default new Tvdata();
